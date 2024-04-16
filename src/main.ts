eb3a4a6272402c7704beeb3ef30567617d737ad0
= <glezgalvan.adrian@gmail.com>
2024-04-16 15:27:48 +0100

    refactor: displays 'FizzBuzz' when number is a multiple of 3 and 5 (using 15)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 export class FizzBuzz {
   public convert(n: number) {
-    if (n % 3 === 0)
-      return n % 5 === 0 ?  'FizzBuzz' : 'Fizz'
-    return n % 5 === 0 ? 'Buzz': n.toString()
+    if (this.isMultipleOf3(n))
+      return this.isMultipleOf5(n) ?  'FizzBuzz' : 'Fizz'
+    return this.isMultipleOf5(n) ? 'Buzz': n.toString()
+  }
+
+  private isMultipleOf3(n: number): boolean {
+    if (n === 0) return true
+    return n > 0 ? this.isMultipleOf3(n - 3) : false
+  }
+
+  private isMultipleOf5(n: number): boolean {
+    if (n === 0) return true
+    return n > 0 ? this.isMultipleOf5(n - 5) : false
   }
 }
