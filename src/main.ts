44f41e494fd9d1e62f9a8158d5f31b9731f9cfdf
= <glezgalvan.adrian@gmail.com>
2024-04-16 15:08:09 +0100

    green: displays 'Fizz' when number is a multiple of 3 (using 6)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 export class FizzBuzz {
   public convert(n: number) {
-    if (n === 3) return 'Fizz'
+    if (n === 3 || n === 6) return 'Fizz'
     if (n === 5) return 'Buzz'
     return n.toString()
   }
