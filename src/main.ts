03a3efa01a326fae3aa136047175aa749961eb48
= <glezgalvan.adrian@gmail.com>
2024-04-16 15:13:36 +0100

    green: displays 'FizzBuzz' when number is a multiple of 3 and 5 (using 15)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 export class FizzBuzz {
   public convert(n: number) {
+    if (n === 15) return 'FizzBuzz'
     if (n % 3 === 0) return 'Fizz'
     if (n % 5 === 0) return 'Buzz'
     return n.toString()
