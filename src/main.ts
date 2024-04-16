ee313cce15441931e7fb3787e3b0e5e7623dfa7e
= <glezgalvan.adrian@gmail.com>
2024-04-16 15:20:20 +0100

    refactor: displays 'FizzBuzz' when number is a multiple of 3 and 5 (using 15)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 export class FizzBuzz {
   public convert(n: number) {
-    if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz'
-    if (n % 3 === 0) return 'Fizz'
-    if (n % 5 === 0) return 'Buzz'
-    return n.toString()
+    if (n % 3 === 0)
+      return n % 5 === 0 ?  'FizzBuzz' : 'Fizz'
+    return n % 5 === 0 ? 'Buzz': n.toString()
   }
 }
