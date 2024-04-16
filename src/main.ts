a338badbc241b420d657eaa0238dfdb1f1621848
= <glezgalvan.adrian@gmail.com>
2024-04-16 15:10:38 +0100

    green: displays 'Buzz' when number is a multiple of 5 (using 10)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 export class FizzBuzz {
   public convert(n: number) {
     if (n % 3 === 0) return 'Fizz'
-    if (n === 5) return 'Buzz'
+    if (n === 5 || n === 10) return 'Buzz'
     return n.toString()
   }
 }
